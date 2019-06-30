23d252943ed5bbcc7a5c8f33a2294791366a81de
Charlton Groves <charltongroves@gmail.com>
2019-06-30 22:09:10 +1000

    ADD: fill rects

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 
 const toRadians = (degree) => degree * (Math.PI / 180)
 const toDegree = (radians) => radians * (180 / Math.PI)
+const distBetween = (p1: [number, number], p2: [number, number]): number =>
+  Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2))
+
 
 // $FlowFixMe
 const c: HTMLCanvasElement = document.getElementById("myCanvas");
@@ -20,56 +23,112 @@ const drawSquare = (ctx: CanvasRenderingContext2D, origin: [number, number], siz
   const ptr = [getmid(tr[0], px), getmid(tr[1], py)]
   const pbl = [getmid(bl[0], px), getmid(bl[1], py)]
   const pbr = [getmid(br[0], px), getmid(br[1], py)]
+
+  // rect left
+  ctx.strokeStyle = '#8A9BAE';
+
+  ctx.fillStyle = '#E6E9ED';
+  ctx.beginPath();
+  ctx.moveTo(...tl);
+  ctx.lineTo(...ptl);
+  ctx.lineTo(...pbl);
+  ctx.lineTo(...bl);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  // rect top
+  ctx.fillStyle = '#CAD2DE';
+  ctx.beginPath();
+  ctx.moveTo(...tl);
+  ctx.lineTo(...ptl);
+  ctx.lineTo(...ptr);
+  ctx.lineTo(...tr);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  // rect bottom
+  ctx.fillStyle = '#CAD2DE';
+  ctx.beginPath();
+  ctx.moveTo(...bl);
+  ctx.lineTo(...pbl);
+  ctx.lineTo(...pbr);
+  ctx.lineTo(...br);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
+  // rect right
+  ctx.fillStyle = '#E6E9ED';
+  ctx.beginPath();
+  ctx.moveTo(...tr);
+  ctx.lineTo(...ptr);
+  ctx.lineTo(...pbr);
+  ctx.lineTo(...br);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+
   // Front square
+  ctx.beginPath();
   ctx.moveTo(...tl);
   ctx.lineTo(...tr);
   ctx.lineTo(...br);
   ctx.lineTo(...bl);
   ctx.lineTo(...tl);
-  // Lines between
-  ctx.lineTo(...ptl);
-  ctx.moveTo(...tr);
-  ctx.lineTo(...ptr);
-  ctx.moveTo(...bl);
-  ctx.lineTo(...pbl);
-  ctx.moveTo(...br);
-  ctx.lineTo(...pbr);
-  // Back square
-  ctx.moveTo(...ptl);
-  ctx.lineTo(...ptr);
-  ctx.lineTo(...pbr);
-  ctx.lineTo(...pbl);
-  ctx.lineTo(...ptl);
+  ctx.stroke();
+  ctx.closePath();
+
 }
+
+type SquareType = {|
+  +origin: [number, number],
+  +midpoint: [number, number],
+  +size: number
+|}
 const range = (start: number, stop: number): number[] => [...Array(stop-start).keys()].map(n => n+start)
 const animateFrame = (ctx: CanvasRenderingContext2D, van: [number, number], distanceFactor: number, it: number) => {
   ctx.clearRect(0, 0, ctx.canvas.height, ctx.canvas.width)
-  ctx.beginPath();
   let fake_x = 0
-  let next_x = -1800 + -1 * ((it*2) % 1600)
+  let next_x = 0
   const init_size = 200
-  range(0,127).map(x => {
+  const squares: SquareType[] = range(0, 31).reduce((acc: SquareType[],x) => {
     const factor_raw = (Math.ceil((fake_x + 1) / 200) - 1) + 4
     const factor = Math.abs(factor_raw % 8 - 4)
     const size_mod = Math.pow(2, factor)
     const size = (init_size / (size_mod))
     const cube_fit = Math.ceil(1001 / size)
     const iter = range(0, cube_fit)
-    iter.map(y => {
-      drawSquare(ctx, [next_x, y * size], size, van, distanceFactor)
+    const cubes = iter.map(y => {
+      return {
+        origin: [next_x, y * size],
+        midpoint: [next_x + (size / 2), y * size + (size / 2)],
+        size: size,
+      }
     })
     next_x += size
     fake_x += size
+    return [...acc, ...cubes]
+  }, [])
+  const sorted_squares = squares.sort((square1: SquareType, square2: SquareType) => {
+    const dist1 = distBetween(square1.midpoint, van)
+    const dist2 = distBetween(square2.midpoint, van)
+    if (dist1 > dist2) {
+      return - 1
+    } else if (dist1 === dist2) {
+      return 0
+    } else {
+      return 1
+    }
   })
-  ctx.stroke();
+  sorted_squares.map((sq: SquareType) => drawSquare(ctx, sq.origin, sq.size, van, distanceFactor))
 }
+
 
 let it = 0
 const render = () => requestAnimationFrame(() => {
   const x = 500 + (300 * Math.cos(toRadians(it)))
   const y = 500 + (300 * Math.sin(toRadians(it)))
-  it += (Math.sin(toRadians(it)) + 1) + 0.1
-  const distanceFactor = (1 + Math.sin(toRadians(it))) / 2
+  const distanceFactor = 0.1
   console.log("vanish x  " + x)
   console.log("vanish y " + y)
   console.log("iter " +  it)
@@ -77,6 +136,7 @@ const render = () => requestAnimationFrame(() => {
   if (it < 1440) {
     render()
   }
+  it += 4
 })
 
 render()
